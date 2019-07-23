a029919bb2aa717d6fb2d8c7e5bf897832c28fc2
Justin Trombley <jtrombley96@gmail.com>
2019-07-23 18:29:35 -0400

    Data fix and Rendering

--- encyclopedia/src/components/Movie.js
+++ encyclopedia/src/components/Movie.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 export default function Movie(props) {
-  console.log('moviejs', props);
-  return (
-    <div>
-      <h1>Marvel Encyclopedia</h1>
-      <h3>{props.title}</h3>
-      {props.releaseDate}
-      {props.description}
-    </div>
-  );
+	console.log('moviejs', props);
+	return (
+		<div>
+			{console.log(props)}
+			<h3>{props.movie.title}</h3>
+			<h5>Release Date: {props.movie.releaseDate}</h5>
+      <h5>Description: {props.movie.description}</h5>
+      <h5>Film Release Order: {props.movie.releaseOrder}</h5>
+      <h5>Film Chronological Order: {props.movie.chronologicalOrder}</h5>
+      <h5>Starring: {props.movie.starring}</h5>
+      <h5>Director: {props.movie.director}</h5>
+      <h5>Phase: {props.movie.phase}</h5>
+		</div>
+	);
 }
